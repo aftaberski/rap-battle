be2bfcfd860db980cde839b1e9b7e35f66de5a32
Anna Taberski <aftaberski@gmail.com>
2015-08-08 15:04:30 -0400

    Add basic search functionality

--- rap-battle/app/controllers/songs_controller.rb
+++ rap-battle/app/controllers/songs_controller.rb
@@ -1,5 +1,11 @@
 class SongsController < ApplicationController
 
   def index
+    Genius.access_token = GENIUS_TOKEN
+    Genius.text_format = "html"
+    if params[:q]
+      song_to_search = params[:q]
+      @song = Genius::Song.search(song_to_search)[0]
+    end
   end
 end
