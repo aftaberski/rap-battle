a3f35d4e968f9175aab2733ebcc7ece14e7ffe40
Anna Taberski <aftaberski@gmail.com>
2015-08-08 16:06:50 -0400

    Add lyrics to the page

--- rap-battle/app/controllers/songs_controller.rb
+++ rap-battle/app/controllers/songs_controller.rb
@@ -6,6 +6,9 @@ class SongsController < ApplicationController
     if params[:q]
       song_to_search = params[:q]
       @song = Genius::Song.search(song_to_search)[0]
+      @lyrics = Nokogiri::HTML(RestClient.get(@song.url)).css("div.lyrics").to_s
+      # .xpath("//text()").to_s
+
     end
   end
 end
